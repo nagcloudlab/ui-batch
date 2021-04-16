a1165cc0933fa36c69b5fedff38de54ce1877c88
Nagabhushanam <nagabhushanamn@gmail.com>
2021-04-16 10:11:32 +0530

    function binding

--- play-with-javascript/index.js
+++ play-with-javascript/index.js
@@ -1,22 +1,3 @@
-
-
-let myFunctions = []
-
-//------------------------------------------------
-
-for (let i = 0; i < 2; i++) {
-    var myFunc = function () {
-        console.log(i)
-    }
-    myFunctions.push(myFunc)
-}
-
-//------------------------------------------------
-
-myFunctions[0]()
-myFunctions[1]()
-
-//------------------------------------------------
-
+"use strict"
 
 
