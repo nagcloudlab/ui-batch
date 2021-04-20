c1e6286c85b5f24c655a1fa46df9cd55a46b445e
Nagabhushanam <nagabhushanamn@gmail.com>
2021-04-20 09:04:08 +0530

    webpack recap

---
+++ app1/src/greeting/en.js
@@ -0,0 +1,8 @@
+
+
+
+function greet() {
+    console.log("good morning...");
+}
+
+export { greet }
---
+++ app1/src/index.js
@@ -0,0 +1,4 @@
+import './styles/style1.css'
+
+import { greet as en } from './greeting/en'
+en();
