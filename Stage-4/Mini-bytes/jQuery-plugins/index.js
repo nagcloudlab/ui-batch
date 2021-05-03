b6b2b0de821be44f387588093d762d2edd7d0488
Nag <nag@NAG-MAC.local>
2021-05-03 15:04:47 +0530

    mini bytes

---
+++ Stage-4/Mini-bytes/jQuery-plugins/index.js
@@ -0,0 +1,21 @@
+
+
+
+$(document).ready(function(){
+
+    // $('#date-field').datepicker({
+    //     showButtonPanel: true
+    // });
+
+    // $( "#dialog" ).hide()
+
+    // $('#msg-btn').click(()=>{
+    //     $( "#dialog" ).dialog();
+    // });
+
+    $('#title')
+    .greenify()
+    .css("font-size",40)
+
+
+})
